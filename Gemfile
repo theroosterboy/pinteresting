962d09a940fcec21956a480b37269acd7a782fd1
Conrad Martin <conrooo@gmail.com>
2013-11-11 20:35:59 -0500

    Implemented Devise For Users

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: '3'
+gem 'devise', '~> 3.1.1'
+
 
 group :development, :test do
 gem 'sqlite3'
@@ -25,4 +27,3 @@ group :doc do
   gem 'sdoc', require: false
 end
 
-#
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for heroku
+  # COme back and put the website name instead of local
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
